a700971dcae7dbea82ba8100e9fd209ba3250410
Angel <blacitea@gmail.com>
2020-08-31 16:37:00 -0700

    add formatSpots func

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,16 @@ const DayListItem = (props) => {
 		"day-list__item--selected": props.selected,
 		"day-list__item--full": props.spots === 0,
 	});
+
+	const formatSpots = () =>
+		props.spots === 1
+			? "1 spot remaining"
+			: `${props.spots ? props.spots : `no`} spots remaining`;
+
 	return (
 		<li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
 			<h2 className="text--regular">{props.name}</h2>
-			<h3 className="text--light">{props.spots}</h3>
+			<h3 className="text--light">{formatSpots()}</h3>
 		</li>
 	);
 };
