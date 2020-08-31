7d81313526a74471cec92b5088ef91a7c717b0d3
Angel <blacitea@gmail.com>
2020-08-31 16:00:20 -0700

    update styling using classNames

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 const DayListItem = (props) => {
+	let dayListItemClass = classNames({
+		"day-list__item": true,
+		"day-list__item--selected": props.selected,
+		"day-list__item--full": props.spots === 0,
+	});
 	return (
-		<li onClick={() => props.setDay(props.name)}>
+		<li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
 			<h2 className="text--regular">{props.name}</h2>
 			<h3 className="text--light">{props.spots}</h3>
 		</li>
