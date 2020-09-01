aec9f9819c5b86ab2e855a4c8edb07bff0e94dbc
Angel <blacitea@gmail.com>
2020-08-31 19:14:05 -0700

    remove console.log

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,6 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 const DayList = (props) => {
 	const dayList = props.days.map((day) => {
-		console.log(day);
-		console.log(day.id);
-		console.log(day.spots);
 		return (
 			<DayListItem
 				key={day.id}
