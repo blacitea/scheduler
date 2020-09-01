5f02ee7c57a7e527c82548eac2dd1d83a56457db
Angel <blacitea@gmail.com>
2020-08-31 17:30:16 -0700

    refactor

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,20 +3,19 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 const DayListItem = (props) => {
+	const { name, selected, spots } = props;
 	let dayListItemClass = classNames({
 		"day-list__item": true,
-		"day-list__item--selected": props.selected,
-		"day-list__item--full": props.spots === 0,
+		"day-list__item--selected": selected,
+		"day-list__item--full": spots === 0,
 	});
 
 	const formatSpots = () =>
-		props.spots === 1
-			? "1 spot remaining"
-			: `${props.spots ? props.spots : `no`} spots remaining`;
+		`${spots ? spots : `no`} ${spots === 1 ? "spot" : "spots"} remaining`;
 
 	return (
-		<li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
-			<h2 className="text--regular">{props.name}</h2>
+		<li className={dayListItemClass} onClick={() => props.setDay(name)}>
+			<h2 className="text--regular">{name}</h2>
 			<h3 className="text--light">{formatSpots()}</h3>
 		</li>
 	);
