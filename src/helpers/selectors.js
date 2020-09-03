f55d2a13cc92fba39ad9c27223b2daec85b8fdb6
Angel <blacitea@gmail.com>
2020-09-02 17:31:26 -0700

    use map instead of forEach

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,12 @@
 const getAppointmentsForDay = (state, day) => {
-	const result = [];
 	const { days, appointments } = state;
 	const selectedDay = days.filter((current) => current.name === day)[0];
 
 	// data validation - return empty array if no data given, or day not found
 	if (!selectedDay) return [];
 
-	const appointmentsid = [...selectedDay.appointments];
-	appointmentsid.forEach((id) => {
-		result.push(appointments[id]);
-	});
+	const appointmentsid = selectedDay.appointments;
+	const result = appointmentsid.map((id) => appointments[id]);
 	return result;
 };
 
