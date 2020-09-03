e75dd252bd8582084cd58a42ab4be7f104e54361
Angel <blacitea@gmail.com>
2020-09-03 13:43:29 -0700

    add option for transition to replace last mode with current

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,16 +3,21 @@ import { useState } from "react";
 const useVisualMode = (defaultVal) => {
 	const [mode, setMode] = useState(defaultVal);
 	const [history, setHistory] = useState([defaultVal]);
-	const transition = (newMode) => {
+
+	const transition = (newMode, option = false) => {
 		setMode(newMode);
-		setHistory([...history, newMode]);
+		option
+			? setHistory([...history.slice(0, -1), newMode])
+			: setHistory([...history, newMode]);
 	};
+
 	const back = () => {
 		if (history.length <= 1) return;
 		const lastMode = history.length - 2;
 		setMode(history[lastMode]);
 		setHistory(history.slice(0, -1));
 	};
+
 	return { mode, transition, back };
 };
 
