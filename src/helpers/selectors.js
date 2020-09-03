c8f974e70ddfde96ee4b2580e419e5333c4bb532
Angel <blacitea@gmail.com>
2020-09-02 21:43:20 -0700

    add selector for interview with test case

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,11 +3,21 @@ const getAppointmentsForDay = (state, day) => {
 	const selectedDay = days.filter((current) => current.name === day)[0];
 
 	// data validation - return empty array if no data given, or day not found
-	if (!selectedDay) return [];
+	return selectedDay
+		? selectedDay.appointments.map((id) => appointments[id])
+		: [];
+	// if (!selectedDay) return [];
 
-	const appointmentsid = selectedDay.appointments;
-	const result = appointmentsid.map((id) => appointments[id]);
-	return result;
+	// const appointmentsid = selectedDay.appointments;
+	// const result = appointmentsid.map((id) => appointments[id]);
+	// return result;
 };
 
 module.exports.getAppointmentsForDay = getAppointmentsForDay;
+
+const getInterview = (state, interview) =>
+	interview
+		? { ...interview, interviewer: state.interviewers[interview.interviewer] }
+		: null;
+
+module.exports.getInterview = getInterview;
