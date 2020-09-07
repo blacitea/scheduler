441a51345bfd97694da42f1da71d71c84517b1c5
Angel <blacitea@gmail.com>
2020-09-07 15:25:15 -0700

    create test file for Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import InterviewerList from "components/InterviewerList";
-import Button from "components/Button";
+import React, { useState } from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
 
-const Form = (props) => {
-	const [name, setName] = useState(props.name || "");
+const Form = props => {
+	const [name, setName] = useState(props.name || '');
 	const [interviewer, setInterviewer] = useState(props.value || null);
 
 	const reset = () => {
-		setName("");
+		setName('');
 		setInterviewer(null);
 	};
 
@@ -21,14 +21,15 @@ const Form = (props) => {
 	return (
 		<main className="appointment__card appointment__card--create">
 			<section className="appointment__card-left">
-				<form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+				<form autoComplete="off" onSubmit={event => event.preventDefault()}>
 					<input
 						value={name}
-						onChange={(event) => setName(event.target.value)}
+						onChange={event => setName(event.target.value)}
 						className="appointment__create-input text--semi-bold"
 						name="name"
 						type="text"
 						placeholder="Enter Student Name"
+						data-testid="student-name-input"
 						/*
           This must be a controlled component
         */
