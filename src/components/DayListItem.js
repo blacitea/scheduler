e12000aef2d139cfcb8f743b61d8d98a407acdc3
Angel <blacitea@gmail.com>
2020-09-08 16:52:00 -0700

    add data-testid

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,20 +1,24 @@
-import React from "react";
-import "components/DayListItem.scss";
-import classNames from "classnames";
+import React from 'react';
+import 'components/DayListItem.scss';
+import classNames from 'classnames';
 
-const DayListItem = (props) => {
+const DayListItem = props => {
 	const { name, selected, spots } = props;
 	let dayListItemClass = classNames({
-		"day-list__item": true,
-		"day-list__item--selected": selected,
-		"day-list__item--full": spots === 0,
+		'day-list__item': true,
+		'day-list__item--selected': selected,
+		'day-list__item--full': spots === 0,
 	});
 
 	const formatSpots = () =>
-		`${spots ? spots : `no`} ${spots === 1 ? "spot" : "spots"} remaining`;
+		`${spots ? spots : `no`} ${spots === 1 ? 'spot' : 'spots'} remaining`;
 
 	return (
-		<li className={dayListItemClass} onClick={() => props.setDay(name)}>
+		<li
+			className={dayListItemClass}
+			onClick={() => props.setDay(name)}
+			data-testid="day"
+		>
 			<h2 className="text--regular">{name}</h2>
 			<h3 className="text--light">{formatSpots()}</h3>
 		</li>
