48da66e246ebdf253fb1bc82ff3679b03b6a1425
Angel <blacitea@gmail.com>
2020-09-08 14:59:15 -0700

    add tests using mock axios, try waitForElement and async/await call

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React from 'react';
 
-import { render, cleanup } from "@testing-library/react";
+import {
+	render,
+	cleanup,
+	waitForElement,
+	fireEvent,
+} from '@testing-library/react';
 
-import Application from "components/Application";
+import Application from 'components/Application';
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe('Application', () => {
+	it('defaults to Monday and changes the schedule when a new day is selected', () => {
+		const { getByText } = render(<Application />);
+
+		return waitForElement(() => getByText('Monday')).then(() => {
+			fireEvent.click(getByText('Tuesday'));
+			expect(getByText('Leopold Silvers')).toBeInTheDocument();
+		});
+	});
+
+	it('aysnc-await: defaults to Monday and changes the schedule when a new day is selected', // async tag the funcion
+	async () => {
+		const { getByText } = render(<Application />);
+
+		await waitForElement(() => getByText('Monday')); // await tag to pause until completion
+
+		// further code will only run after prev await completed
+		fireEvent.click(getByText('Tuesday'));
+		expect(getByText('Leopold Silvers')).toBeInTheDocument();
+	});
+});
+
+xit('renders without crashing', () => {
+	render(<Application />);
 });
