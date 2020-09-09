d9c53b1dd4eedbdf0c42f50d9e16798c59cc6fda
Angel <blacitea@gmail.com>
2020-09-08 23:34:36 -0700

    add test for post save button press

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,12 +1,25 @@
 beforeEach(() => {
 	cy.log('I run before every test in every spec file!');
-	cy.visit('/api/debug/reset');
+	cy.request('GET', '/api/debug/reset');
 });
 
 describe('Appointment', () => {
 	it('should book an interview', () => {
 		cy.visit('/').contains('Monday');
+
+		// use .first() because 2nd button hidden by css style
 		cy.get('[alt=Add]').first().click();
+
 		cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+
+		cy.get('[alt="Sylvia Palmer"]').click();
+
+		cy.contains('Save').first().click();
+
+		cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+		cy.contains('.appointment__card--show', 'Sylvia Palmer');
+
+		// cy.contains('Lydia Miller-Jones'); // wait till student name displayed
+		// cy.get('.appointment__card--show').eq(1).contains('Sylvia Palmer'); // check for class name and interviewer name display
 	});
 });
