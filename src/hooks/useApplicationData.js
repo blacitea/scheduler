f8d94fcce8e62979754ab5da69fd9b3bdf6ac6b6
Angel <blacitea@gmail.com>
2020-09-09 12:13:20 -0700

    comment SET_DAY to break the app and test CircleCI

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,8 +8,8 @@ const useApplicationData = () => {
 
 	const reducer = (state, action) => {
 		switch (action.type) {
-			case SET_DAY:
-				return { ...state, day: action.day };
+			// case SET_DAY:
+			// 	return { ...state, day: action.day };
 			case SET_APPLICATION_DATA:
 				return {
 					...state,
