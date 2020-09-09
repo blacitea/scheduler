66b1a50dfc479e73c26008c2eedf48098cc3a6c8
Angel <blacitea@gmail.com>
2020-09-08 22:07:26 -0700

    add test for navigate to root and navigate to Tuesday

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,21 @@
+const { testHook } = require('react-hooks-testing-library');
+const { CYCLIC_KEY } = require('@storybook/addon-actions');
+
+describe('Navigation', () => {
+	it('should visit root', () => {
+		cy.visit('/');
+	});
+
+	it('should navigation to Tuesday', () => {
+		cy.visit('/');
+		// cy.get('li').contains('Tuesday').click();
+		// cy.contains('li', 'Tuesday').should(
+		// 	'have.class',
+		// 	'day-list__item--selected'
+		// );
+		cy.contains('[data-testid=day]', 'Tuesday') // use data-testid / data-cy over element tag
+			.click()
+			.should('have.class', 'day-list__item--selected');
+		// .should('have.css', 'background-color', 'rgb(242, 242, 242)'); // bad practice - css are very changable
+	});
+});
